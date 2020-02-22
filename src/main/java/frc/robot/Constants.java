dba76b3fdc99d220d78e7ae558becb4e69675775
Blakester609 <blaked609@gmail.com>
2020-02-22 14:23:36 -0700

    Transfered stuff from characterization project into robot code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -21,6 +21,9 @@ public final class Constants {
         public static final int motor2 = 2;
         public static final int motor3 = 1;
         public static final int motor4 = 3;
+        public static final double WHEEL_DIAMETER = 139.27;
+        public static final double ENCODER_EDGES_PER_REV = 2048;
+        public static final int PIDIDX = 0;
     }
     public static final class OI {
         public static final int joyPort = 0;
